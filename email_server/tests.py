5bbfa305eb821e701d586ecfcb109bb8c8d20467
Erik Brown <erik@altschool.com>
2021-03-06 15:36:29 -0800

    adds email and missing fields validation

--- email_server/tests.py
+++ email_server/tests.py
@@ -5,6 +5,7 @@ from django.test import (
 
 
 HTTP_200_OK = 200
+HTTP_400_BAD_REQUEST = 400
 HTTP_405_NOT_ALLOWED = 405
 
 
@@ -18,6 +19,19 @@ class PostEmailAPITestCase(TestCase):
     def tearDown(self):
         super(PostEmailAPITestCase, self).tearDown()
 
+    def create_email(self, **kwargs):
+        email = {
+            'to': 'susan@abcpreschool.org',
+            'to_name': 'Miss Susan',
+            'from': 'noreply@mybrightwheel.com',
+            'from_name': 'brightwheel',
+            'subject': 'Your Weekly Report',
+            'body': '<h1>Weekly Report</h1><p>You saved 10 hours</p>',
+        }
+
+        email.update(kwargs)
+        return email
+
     def test_does_not_accept_get_delete_put(self):
         get_response = self.client.get(self.emails_endpoint)
         self.assertEqual(get_response.status_code, HTTP_405_NOT_ALLOWED)
@@ -27,3 +41,48 @@ class PostEmailAPITestCase(TestCase):
 
         delete_response = self.client.delete(self.emails_endpoint)
         self.assertEqual(delete_response.status_code, HTTP_405_NOT_ALLOWED)
+
+    def test_returns_200_ok_when_email_post_validates(self):
+        response = self.client.post(
+            self.emails_endpoint,
+            data=self.create_email()
+        )
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def test_returns_400_with_invalid_email(self):
+        email = self.create_email(to='not-an-email-address')
+
+        response = self.client.post(self.emails_endpoint, data=email)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.content.decode('utf-8'),
+            'Invalid "to" email address'
+        )
+
+        email = self.create_email(**{'from': 'not-an-email-address'})
+
+        response = self.client.post(self.emails_endpoint, data=email)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.content.decode('utf-8'),
+            'Invalid "from" email address'
+        )
+
+    def test_returns_400_with_missing_parameters(self):
+        email = self.create_email()
+        del email['to']
+
+        response = self.client.post(self.emails_endpoint, data=email)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.content.decode('utf-8'),
+            'Missing required: to'
+        )
+
+        del email['from_name']
+        response = self.client.post(self.emails_endpoint, data=email)
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.content.decode('utf-8'),
+            'Missing required: from_name, to'
+        )
--- email_server/views.py
+++ email_server/views.py
@@ -1,11 +1,37 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 from django.http import HttpResponse
 
 
+HTTP_400_BAD_REQUEST = 400
 HTTP_405_METHOD_NOT_ALLOWED = 405
+
+
+def validate_email(data):
+    required_fields = {'to', 'to_name', 'from', 'from_name', 'subject', 'body'}
+    validators = {
+        'to': EmailValidator('Invalid "to" email address'),
+        'from': EmailValidator('Invalid "from" email address'),
+    }
+
+    missing_fields = required_fields - set(data.keys())
+
+    if missing_fields:
+        msg = 'Missing required: {}'.format(', '.join(sorted(missing_fields)))
+        raise ValidationError(msg)
+
+    for field, validator in validators.items():
+        validator(data.get(field))
+
+    return data
 
 
 def handle_email_request(request):
     if request.method != 'POST':
         return HttpResponse(status=HTTP_405_METHOD_NOT_ALLOWED)
 
-    return HttpResponse()
+    try:
+        email = validate_email(request.POST)
+        return HttpResponse()
+    except ValidationError as error:
+        return HttpResponse(error.message, status=HTTP_400_BAD_REQUEST)
